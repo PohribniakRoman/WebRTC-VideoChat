a6a611521151b9d2ab1af11196b01c9fad01595f
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-07-28 15:32:35 +0300

    reset

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,5 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { v4 } from 'uuid';
-import socket from "../socket";
-//@ts-ignore
-import {ACTIONS} from "../socket/actions.js"
-
 export const Home:React.FC = ()=>{
-    const [activeRooms,setRooms] = useState<string[]>([]);
-    const navigate = useNavigate()
-    useEffect(()=>{
-        socket.on(ACTIONS.SHARE_ROOMS,({rooms}:Record<string,string[]>)=>{
-            setRooms(rooms);
-        })
-    })
+
     return<>
-        <ul>
-        {activeRooms.map(roomID=>{
-            return <li key={roomID}>{roomID}<button onClick={()=>{navigate(`/room/${roomID}`)}}>Join</button></li>
-        })}
-        </ul>
-        <button onClick={()=>{
-            navigate(`/room/${v4()}`)
-        }}>Create Room</button>
     </>
 }
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,22 +1,6 @@
-import { useCallback, useState } from "react";
-import { useEffect, useRef } from "react";
-import socket from "../socket";
-//@ts-ignore
-import {ACTIONS} from "../socket/actions.js"
-import { useParams } from "react-router-dom";
-import { useWebRTC } from "../hooks/useWebRTC";
-import { validate,version } from "uuid";
 
 export const Room:React.FC = ()=>{
-    const {id:roomID} = useParams();
-    if(!roomID || !validate(roomID) || version(roomID) !==  4){
-        return <>Eror</>
-    }
-    const {clients,assignRef} = useWebRTC(roomID);
 
     return<div>
-        {clients.map((peerID:string)=>{
-            return <video key={peerID} ref={(element)=>assignRef(peerID,element)} autoPlay playsInline/>
-        })}
     </div>
 }
